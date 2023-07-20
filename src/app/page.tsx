ed5770ffaa72fee9bba192d9aafe37c04b54b7b8
Michael Pacheco <michael.dias@luizalabs.com>
2023-07-20 13:21:16 -0300

    feat: add header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div></div>;
-}
+import Header from "@/app/containers/Header";
+import { NextPage } from "next";
+
+const Home: NextPage = () => (
+  <Header
+    name="Michael Pacheco"
+    role="Senior Software Engineer"
+    imageProps={{
+      width: 144,
+      height: 144,
+      src: "https://placekitten.com/800/800",
+      alt: "Michael Pacheco's picture",
+      priority: true,
+    }}
+  />
+);
+
+export default Home;
---
+++ src/components/Image.tsx
@@ -0,0 +1,4 @@
+import Image from "next/image";
+
+export * from "next/image";
+export default Image;
