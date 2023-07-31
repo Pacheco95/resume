09222f2226349b7346dcd41f4ce5e88516df232c
Michael Pacheco <michael.dias@luizalabs.com>
2023-07-31 14:27:20 -0300

    feat: forward ref `ExternalLink`

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,14 +1,24 @@
-import { AnchorHTMLAttributes } from "react";
+import { forwardRef, type JSX } from "react";
 
-import cn from "classnames";
+type Anchor = JSX.IntrinsicElements["a"];
 
-interface ExternalLinkProps
-  extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "target"> {}
+import { cn } from "@/utils";
 
-const ExternalLink = ({ className, children, ...props }: ExternalLinkProps) => (
-  <a {...props} target="_blank" className={cn("underline", className)}>
-    {children}
-  </a>
+interface ExternalLinkProps extends Omit<Anchor, "target"> {}
+
+const ExternalLink = forwardRef<HTMLAnchorElement, ExternalLinkProps>(
+  function ExternalLinkForwardRef({ className, children, ...props }, ref) {
+    return (
+      <a
+        ref={ref}
+        {...props}
+        target="_blank"
+        className={cn("underline", className)}
+      >
+        {children}
+      </a>
+    );
+  },
 );
 
 export default ExternalLink;
